55ce3c728b022260c1c525be76ccaf61cd6bee30
Gunns4Hire <gunnarrachau@gmail.com>
2025-11-09 07:57:40 -0600

    Pretty print

--- main.py
+++ main.py
@@ -1,19 +1,39 @@
+# Define initial function
 def get_book_text(filepath):
     with open(filepath, "r") as f:
         file_contents = f.read()
     return file_contents
-
-from stats import count_words
-from stats import count_characters
-
-
+# Inport stats from stats.py
+from stats import count_words, count_characters
+# Sort def
+def sort_on(item):
+    return item["num"]
+# Format for new output(pretty text)
 def main():
+    print("============== BOOKBOT ===============")
+    print("Analyzing book found at books/frankenstein.txt...")
+    print("----------- Word Count -----------")
+
     book_text = get_book_text("books/frankenstein.txt")
     num_words = count_words(book_text)
-    chars = count_characters(book_text)
+    chars_dict = count_characters(book_text)
+        # pretty print 
     print(f"Found {num_words} total words")
-    print(chars)
+    print("----------- Character Count -------")
 
+    # Convert dict to list of dicts for sorting 
+    chars_list = []
+    for char, num in chars_dict.items():
+            #alpha chars only 
+            if char.isalpha():
+                 chars_list.append({"char": char, "num": num})
+
+    # Sort in descending order by "num"
+    chars_list.sort(reverse=True, key=sort_on)
+
+    # Print each character and count
+    for item in chars_list:
+        print(f"{item['char']}: {item['num']}")
 
 if __name__ == "__main__":
     main()
